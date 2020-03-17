4ff4a22bc393fd03577d2e8992199d04533c2f5f
Joanne Qiong Sun <joannesun44@gmail.com>
2020-03-17 16:34:43 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -18,7 +18,7 @@ attr_accessor :name, :years_experience
   end 
   
   def new_meal(customer, total, tip=0)
-    Meal.new(customer, self, total, tip)
+    Meal.new(self, customer, total, tip)
   end 
   
   def meals
